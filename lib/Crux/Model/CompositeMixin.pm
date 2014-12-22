bb76e41ac2e7d2e7b20f811dd51676b14303cc05
Adam Helybely <adam@helybely.com>
2014-12-22 11:55:11 +0100

    Composite class support added.

---
+++ lib/Crux/Model/CompositeMixin.pm
@@ -0,0 +1,38 @@
+#! /usr/bin/perl
+###### NAMESPACE ##############################################################
+
+package Crux::Model::CompositeMixin;
+
+###### IMPORTS ################################################################
+
+use Essence::Strict;
+
+use parent qw( Blueprint::Composite );
+
+use Essence::Merge;
+
+###### METHODS ################################################################
+
+sub _api_id_cols
+{
+  my ($class, @rest) = @_;
+  return @{Essence::Merge::merge_arrays(
+      map { [$_->_api_id_cols(@rest)] }
+          @{$class->get_metaclass()->GetConfig(':components')})};
+}
+
+sub api_load_by_id
+{
+  # my ($class, $s, $id, $action, $opts) = @_;
+  my ($class, $s, $id, $action, @rest) = @_;
+  return $class->assemble($s,
+      map {
+            $_->api_load_by_id($s,
+                $_->api_extract_verify_id($s, $action, $id),
+                $action, @rest)
+          } @{$class->get_metaclass()->GetConfig(':components')});
+}
+
+###############################################################################
+
+1
